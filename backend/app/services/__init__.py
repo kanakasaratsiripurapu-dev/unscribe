0dc9897275668f207d74ccb7ec0181a302092f74
kanakasaratsiripurapu-dev <kanakasarat.siripurapu@sjsu.edu>
2025-12-16 19:07:24 -0800

    Add __init__.py

---
+++ backend/app/services/__init__.py
@@ -0,0 +1,7 @@
+"""Service modules"""
+from .gmail_service import gmail_service
+from .detection_service import detection_service
+from .unsubscribe_service import unsubscribe_service
+
+__all__ = ["gmail_service", "detection_service", "unsubscribe_service"]
+
